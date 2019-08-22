c7c32a1bd77670bd73ec82c762f58c96722b8980
baldm0mma <jev.forsberg@gmail.com>
2019-08-22 12:31:49 -0600

    Add production data to knexfile
    Co-authored-by: Evan Markowitz <emarkowitz1@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -18,5 +18,15 @@ module.exports = {
     seeds: {
       directory: './db/seeds/test'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
   }
 };
